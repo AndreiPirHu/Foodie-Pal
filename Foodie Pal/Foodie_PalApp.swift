5bbbde6770eef2c57369f969ce9e8b5ffbfb9adc
Andrei Pirogov Huertas <andrei.pirogov.huertas@gmail.com>
2023-01-25 07:56:38 +0100

    Initial Commit

---
+++ Foodie Pal/Foodie_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foodie_PalApp.swift
+//  Foodie Pal
+//
+//  Created by Andrei Pirogov on 2023-01-25.
+//
+
+import SwiftUI
+
+@main
+struct Foodie_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
