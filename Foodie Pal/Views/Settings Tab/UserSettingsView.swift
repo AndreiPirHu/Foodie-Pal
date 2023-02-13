d1bc244eafd7d6f8bb83e97e92772dfb8ebcb1a0
Andrei Pirogov Huertas <andrei.pirogov.huertas@gmail.com>
2023-02-13 18:12:59 +0100

    Added schedule times to Firestore and to user settings so that they can be changed. Fixed sheet on map so that it displays information correctly the first time clicked

--- Foodie Pal/Views/Settings Tab/UserSettingsView.swift
+++ Foodie Pal/Views/Settings Tab/UserSettingsView.swift
@@ -13,7 +13,21 @@ import FirebaseFirestore
 struct UserSettingsView: View {
     @State var userSettings = [UserSettings]()
     @State var isLoggedOut = false
+    
+    @State var selectedTime = Date()
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
     var sendSetting = ["description", "email", "name", "address"]
+    
     
     let db = Firestore.firestore()
     
@@ -22,25 +36,49 @@ struct UserSettingsView: View {
             VStack{
                 ForEach(userSettings) { setting in
                     
+                    Text("Hello, \(setting.name)")
+                        //.padding(.bottom, 10)
+                        .padding(.top, 30)
+                        .font(.title)
+                    
+                    Spacer()
+                    
                     NavigationView{
                         List{
                             NavigationLink(destination: UserSettingsEditView(receivedSetting: sendSetting[0])){
-                                ExtractedView(setting: "Beskrivning:", settingText: setting.description)
+                                ListedSettingView(setting: "Beskrivning:", settingText: setting.description)
                             }
-                            NavigationLink(destination: UserSettingsEditView(receivedSetting: sendSetting[1])){
-                                ExtractedView(setting: "Email:", settingText: setting.email)
-                            }
+                            //NavigationLink(destination: UserSettingsEditView(receivedSetting: sendSetting[1])){
+                            //    ListedSettingView(setting: "Email:", settingText: setting.email)
+                            //}
                             NavigationLink(destination: UserSettingsEditView(receivedSetting: sendSetting[2])){
-                                ExtractedView(setting: "Namn:", settingText: setting.name)
+                                ListedSettingView(setting: "Namn:", settingText: setting.name)
                             }
                             NavigationLink(destination: UserSettingsEditView(receivedSetting: sendSetting[3])){
-                                ExtractedView(setting: "Adress:", settingText: setting.address)
+                                ListedSettingView(setting: "Adress:", settingText: setting.address)
                             }
-                        }
+                            ListedScheduleView(weekday: "Mån", openingTime: setting.schedMonOpen , closingTime: setting.schedMonClose, weekdayOpening: "schedMonOpen", weekdayClosing: "schedMonClose")
+                            
+                            ListedScheduleView(weekday: "Tis", openingTime: setting.schedTueOpen , closingTime: setting.schedTueClose, weekdayOpening: "schedTueOpen", weekdayClosing: "schedTueClose")
+                            
+                            ListedScheduleView(weekday: "Ons", openingTime: setting.schedWedOpen , closingTime: setting.schedWedClose, weekdayOpening: "schedWedOpen", weekdayClosing: "schedWedClose")
+                            
+                            ListedScheduleView(weekday: "Tors", openingTime: setting.schedThuOpen , closingTime: setting.schedThuClose, weekdayOpening: "schedThuOpen", weekdayClosing: "schedThuClose")
+                            
+                            ListedScheduleView(weekday: "Fre", openingTime: setting.schedFriOpen , closingTime: setting.schedFriClose, weekdayOpening: "schedFriOpen", weekdayClosing: "schedFriClose")
+                            
+                            ListedScheduleView(weekday: "Lör", openingTime: setting.schedSatOpen , closingTime: setting.schedSatClose, weekdayOpening: "schedSatOpen", weekdayClosing: "schedSatClose")
+                            
+                            ListedScheduleView(weekday: "Sön", openingTime: setting.schedSunOpen , closingTime: setting.schedSunClose, weekdayOpening: "schedSunOpen", weekdayClosing: "schedSunClose")
+                            
+                        }.scrollContentBackground(.hidden)
+                            .navigationTitle("Inställningar")
                     }
-                    .navigationTitle("Hello, \(setting.name)")
+                    
+                    
                 }
-                Spacer()
+
+                
                 Button(action: {
                     logOut()
                 }) {
@@ -49,9 +87,13 @@ struct UserSettingsView: View {
                         .bold()
                         .frame(width: 200,height: 40)
                         .background(
-                            RoundedRectangle(cornerRadius: 10, style: .continuous))
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                .fill(Color.red)
+                        )
                         
+                        .padding(.bottom, 40)
                 }
+                
                 
             }
             .onAppear() {
@@ -61,6 +103,13 @@ struct UserSettingsView: View {
                 SettingsView().navigationBarBackButtonHidden(true)
             }
     }
+    
+    
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter
+    }()
     
     func logOut() {
         do {
@@ -90,11 +139,39 @@ struct UserSettingsView: View {
                 if let description = data?["description"] as? String,
                    let email = data?["email"] as? String,
                    let name = data?["name"] as? String,
-                   let address = data?["address"] as? String {
+                   let address = data?["address"] as? String,
+                   let schedMonOpen = data?["schedMonOpen"] as? String,
+                   let schedMonClose = data?["schedMonClose"] as? String,
+                    let schedTueOpen = data?["schedTueOpen"] as? String,
+                    let schedTueClose = data?["schedTueClose"] as? String,
+                    let schedWedOpen = data?["schedWedOpen"] as? String,
+                    let schedWedClose = data?["schedWedClose"] as? String,
+                   let schedThuOpen = data?["schedThuOpen"] as? String,
+                   let schedThuClose = data?["schedThuClose"] as? String,
+                   let schedFriOpen = data?["schedFriOpen"] as? String,
+                   let schedFriClose = data?["schedFriClose"] as? String,
+                   let schedSatOpen = data?["schedSatOpen"] as? String,
+                   let schedSatClose = data?["schedSatClose"] as? String,
+                   let schedSunOpen = data?["schedSunOpen"] as? String,
+                   let schedSunClose = data?["schedSunClose"] as? String {
                     let userSetting = UserSettings(description: description,
                                                    email: email,
                                                    name: name,
-                                                   address: address)
+                                                   address: address,
+                                                   schedMonOpen: schedMonOpen,
+                                                   schedMonClose: schedMonClose,
+                                                   schedTueOpen: schedTueOpen,
+                                                   schedTueClose: schedTueClose,
+                                                   schedWedOpen: schedWedOpen,
+                                                   schedWedClose: schedWedClose,
+                                                   schedThuOpen: schedThuOpen,
+                                                   schedThuClose: schedThuClose,
+                                                   schedFriOpen: schedFriOpen,
+                                                   schedFriClose: schedFriClose,
+                                                   schedSatOpen: schedSatOpen,
+                                                   schedSatClose: schedSatClose,
+                                                   schedSunOpen: schedSunOpen,
+                                                   schedSunClose: schedSunClose)
                     userSettings.append(userSetting)
                     
                 }
@@ -108,7 +185,7 @@ struct UserSettingsView: View {
 
 
 
-struct ExtractedView: View {
+struct ListedSettingView: View {
     var setting : String
     var settingText : String
     
@@ -120,6 +197,76 @@ struct ExtractedView: View {
             Text(settingText.prefix(7) + "...")
         }
     }
+}
+
+
+struct ListedScheduleView: View {
+    var weekday: String
+    @State var openingTime: String
+    
+    @State var closingTime: String
+    
+    var weekdayOpening: String
+    var weekdayClosing: String
+    
+    let times = ["Stängt","00:30", "01:00", "01:30", "02:00", "02:30", "03:00", "03:30", "04:00", "04:30", "05:00", "05:30", "06:00", "06:30", "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30", "23:00", "23:30", "00:00"]
+    
+    var body: some View {
+        HStack{
+            Text(weekday)
+            
+            Spacer()
+            
+            Picker("Välj en tid", selection: $openingTime){
+                ForEach(times, id: \.self) { time in
+                    Text(time)
+                }
+            }
+            .frame(width: 100)
+            .labelsHidden()
+            //triggers the updateschedule func when a change is made
+            .onReceive([self.openingTime].publisher.first()) { (value) in
+                            self.updateSchedule()
+                        }
+            
+            Text("-")
+            
+            Picker("Välj en tid", selection: $closingTime){
+                ForEach(times, id: \.self) { time in
+                    Text(time)
+                }
+            }
+            .frame(width: 100)
+            .labelsHidden()
+            //triggers the updateschedule func when a change is made
+            .onReceive([self.closingTime].publisher.first()) { (value) in
+                            self.updateSchedule()
+                        }
+                
+        }
+        
+        
+        
+        
+    }
+    //updates the schedule times in firestore when they are changed in the picker
+    func updateSchedule() {
+        let db = Firestore.firestore()
+        guard let userUid = Auth.auth().currentUser?.uid else {return}
+
+        let docRef = db.collection("users").document(userUid)
+
+        docRef.updateData(["\(weekdayOpening)": openingTime,
+                           "\(weekdayClosing)": closingTime ]) { error in
+            if let error = error {
+                print("Error updating document: \(error)")
+            } else {
+                print("Document successfully updated!")
+            }
+        }
+    }
+    
+    
 }
 
 
