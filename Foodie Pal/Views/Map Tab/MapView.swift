931634e34878595d08e5ec13f58b01cb46c96d17
Andrei Pirogov Huertas <andrei.pirogov.huertas@gmail.com>
2023-02-20 18:05:19 +0100

    Added function to open address in apple maps from sheet and list Changed position of the image gallery in sheet and list to top

--- Foodie Pal/Views/Map Tab/MapView.swift
+++ Foodie Pal/Views/Map Tab/MapView.swift
@@ -253,8 +253,30 @@ struct FoodTruckSheetView: View {
                         .frame(width: 30, height: 30)
                         .foregroundColor(.gray)
                 }
-            }
+            }.padding(.bottom, -10)
             Text(foodTruck.category)
+                .padding(.bottom, 20)
+            
+            // image gallery
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack{
+                    ForEach(downloadedImages) { image in
+                        
+                        imagePreView(image: image.image)
+                            .padding(-1)
+
+                    }
+                }
+                
+                //sheet for the expanded image view
+            }.sheet(isPresented: $imageExpanderPresented, onDismiss: {imageExpanderPresented = false}) {
+                ExpandedImageGallerySheetView(imageExpanderPresented: $imageExpanderPresented, foodTruckName: foodTruck.name, downloadedImages: downloadedImages)
+            }// end of sheet for image gallery expander
+            //ontap opens up the expanded image view
+            .onTapGesture {
+                imageExpanderPresented = true
+            }
+            .padding(.bottom, 20)
             
             
             
@@ -298,29 +320,32 @@ struct FoodTruckSheetView: View {
                     .background(.gray)
                     .frame(width: 340)
                 
-                Text(foodTruck.address)
-                    .padding(.bottom, 20)
-                
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    
-                    HStack{
-                        ForEach(downloadedImages) { image in
-                            
-                            imagePreView(image: image.image)
-                                .padding(-1)
-
-                        }
+                Button(action: {
+                            openInMaps()
+                }){
+                    HStack {
+                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            .foregroundColor(Color.blue)
+                        
+                            .frame(height: 30)
+                            .overlay(
+                                Text(foodTruck.address).foregroundColor(.white)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(.leading, 16)
+                            )
+                        
+                        Image(systemName: "arrow.triangle.turn.up.right.circle.fill")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .offset(x: -40)
+                            .foregroundColor(.white)
+                        
                     }
-                    
-                    //sheet for the expanded image view
-                }.sheet(isPresented: $imageExpanderPresented, onDismiss: {imageExpanderPresented = false}) {
-                    ExpandedImageGallerySheetView(imageExpanderPresented: $imageExpanderPresented, foodTruckName: foodTruck.name, downloadedImages: downloadedImages)
-                }// end of sheet for image gallery expander
-                //ontap opens up the expanded image view
-                .onTapGesture {
-                    imageExpanderPresented = true
+                    .padding(.bottom, 20)
                 }
+                
+                
+                
                 
                 Text("BESKRIVNING")
                     .font(.custom("", size: 14))
@@ -357,6 +382,19 @@ struct FoodTruckSheetView: View {
         }
         .padding()
         .padding(.top, 15)
+    }
+    
+    func openInMaps(){
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(foodTruck.address) { (placemarks, error) in
+                   if let error = error {
+                       print("Geocoding error: \(error.localizedDescription)")
+                   } else if let placemark = placemarks?.first {
+                       let mapItem = MKMapItem(placemark: MKPlacemark(placemark: placemark))
+                       mapItem.name = foodTruck.address
+                       mapItem.openInMaps()
+                   }
+               }
     }
         
     func updateMessagesFromFirestore() {
